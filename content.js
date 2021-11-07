b2d8e66ee63820332addfe634188e6cbad57d6f0
Endiasz <endiaszpl@gmail.com>
2021-11-08 00:17:12 +0100

    Update content.js

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ console.log("Script go")
 
 chrome.runtime.onMessage.addListener(gotMesssage);
 
-function gotMesssage(request, sender, sendResponse) {
-    console.log(request.txt);
+function gotMesssage(message, sender, sendResponse) {
+    console.log(message.txt);
 }
 

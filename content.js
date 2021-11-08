f6f0fb78ec1e6e8775c3ce4a43e1f69b1e48515d
Endiasz <85460611+Endiasz@users.noreply.github.com>
2021-11-08 09:26:39 +0100

    Update content.js

--- content.js
+++ content.js
@@ -3,13 +3,19 @@ console.log("Script go")
 chrome.runtime.onMessage.addListener(gotMesssage);
 
 function gotMesssage(message, sender, sendResponse) {
-
+    console.log(message);
     if (message.destination === undefined) {
         return;
     }
 
-    if (message.destination === "getTranslations") {
-        sendResponse(translations)
+
+    if (message.active === undefined) {
+
+    } else if (message.active === true) {
+        console.log("StartBot");
+    }
+    else if (message.active === false) {
+        console.log("StopBot");
     }
 }
 
